28409413f95b03088d1221f4f672fbae8511e1a0
joaoMarinho94 <joaopaulim94@gmail.com>
2021-02-26 00:57:12 -0300

    Created controller, repository, model, migration and routes to surveyUser

---
+++ api/src/controllers/SendMailController.ts
@@ -0,0 +1,39 @@
+import { Request, Response } from "express";
+import { getCustomRepository } from "typeorm";
+
+import { SurveysRepository } from "../repositories/SurveysRepository";
+import { SurveysUsersRepository } from "../repositories/SurveysUsersRepository";
+import { UserRepository } from "../repositories/UserRepository";
+
+class SendMailController {
+  async execute(req: Request, res: Response): Promise<any> {
+    try {
+      const { email, survey_id } = req.body;
+
+      const userRepository = getCustomRepository(UserRepository);
+      const surveysRepository = getCustomRepository(SurveysRepository);
+      const surveysUsersRepository = getCustomRepository(SurveysUsersRepository);
+
+      const userAlreadyExists = await userRepository.findOne({ email });
+
+      if (!userAlreadyExists) { return res.status(400).json({ message: "Usuário não encontrado." }); }
+
+      const surveyAlreadyExists = await surveysRepository.findOne({ id: survey_id });
+
+      if (!surveyAlreadyExists) { return res.status(400).json({ message: "Enquete não encontrada." }); }
+
+      const surveyUser = surveysUsersRepository.create({
+        user_id: userAlreadyExists.id,
+        survey_id,
+      });
+      await surveysUsersRepository.save(surveyUser);
+
+      return res.json(surveyUser);
+    } catch (error) {
+      console.log("error: ", error);
+      return res.status(500).json({ message: "Ocorreu um erro ao enviar o email." });
+    }
+  }
+}
+
+export { SendMailController };
--- api/src/routes.ts
+++ api/src/routes.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 
+import { SendMailController } from "./controllers/SendMailController";
 import { SurveysController } from "./controllers/SurveysController";
 import { UserController } from "./controllers/userController";
 
@@ -7,6 +8,7 @@ const router = Router();
 
 const userController = new UserController();
 const surveyController = new SurveysController();
+const sendMailController = new SendMailController();
 
 router.post("/users", userController.create);
 router.get("/user/:id", userController.show);
@@ -16,4 +18,5 @@ router.post("/surveys", surveyController.create);
 router.get("/survey/:id", surveyController.show);
 router.get("/surveys", surveyController.index);
 
+router.post("/sendMail", sendMailController.execute);
 export { router };
