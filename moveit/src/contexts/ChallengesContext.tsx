443e2fff8dd164f231696ab7743cc3a3500d46d3
joaoMarinho94 <joaopaulim94@gmail.com>
2021-02-26 21:13:26 -0300

    Added notification and audio to new challenge

--- moveit/src/contexts/ChallengesContext.tsx
+++ moveit/src/contexts/ChallengesContext.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, ReactNode, useState } from 'react'
+import React, { createContext, ReactNode, useEffect, useState } from 'react'
 import challenges from '../../challenges.json'
 
 interface ChallengesProviderProps {
@@ -33,6 +33,10 @@ export function ChallengesProvider({ children }): ChallengesProviderProps {
 
   const experienceToNextLevel = Math.pow((level + 1) * 4, 2)
 
+  useEffect(() => {
+    Notification.requestPermission()
+  }, [])
+
   function levelUp() {
     setLevel(level + 1)
   }
@@ -41,6 +45,14 @@ export function ChallengesProvider({ children }): ChallengesProviderProps {
     const randomChallengeIndex = Math.floor(Math.random() * challenges.length)
     const challenge = challenges[randomChallengeIndex]
     setActiveChallenge(challenge)
+
+    new Audio('/notification.mp3').play()
+
+    if (Notification.permission === 'granted') {
+      new Notification('Novo Desafio', {
+        body: `Valendo ${challenge.amount}xp!`
+      })
+    }
   }
 
   function resetChallenge() {
